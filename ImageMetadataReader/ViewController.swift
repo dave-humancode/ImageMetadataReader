e81c017fd79713419cba7f5af1122e694a5a8fd2
Dave Rahardja <dave@humancode.us>
2023-06-16 10:39:59 -0700

    Initial Commit

---
+++ ImageMetadataReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageMetadataReader
+//
+//  Created by Dave Rahardja on 6/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
